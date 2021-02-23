b0e6df9e10572ff6210299ec0c363ad94c1b1049
EarthGodzilla <smneason@gmail.com>
2021-02-23 17:11:05 -0500

    Third edit commit

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -17,30 +17,44 @@ public class AddressBook {
                     case 0:
                         System.exit(0);
                     case 1:
-                        while (menu != 2) {
+                        while (menu == 1){
                             System.out.println("Enter Your name");
                             String fullName=sc.next();
                             System.out.println("Enter Your email");
                             String email=sc.next();
                             Person person=new Person(fullName,email);
                             person.setName(fullName);
-                            person.setEmail(fullName);
-                            System.out.println("you just entered"+person.toString());
+                            person.setEmail(email);
+                            System.out.println("you just entered "+person.toString());
                             addressbook.put(fullName,email);
-                            System.out.println("would you like to add someone else? 1: yes, 2: No");
+                            System.out.println("would you like to add someone else? 1: yes, 4: No");
                             menu=sc.nextInt();
 
                         }
                         break;
                     case 2:
-                        System.out.println("Enetr the person you would like to look up:");
-                        String fullNameKey=sc.next();
-                        System.out.println("email: "+addressbook.get(fullNameKey));
+                        while (menu == 2) {
+                            System.out.println("Enter the person you would like to look up:");
+                            String fullNameKey = sc.next();
+                            System.out.println("email: " + addressbook.get(fullNameKey));
+                            System.out.println("would you like to look someone else up? 2: yes, 4: No");
+                            menu = sc.nextInt();
+                        }
                         break;
                     case 3:
-                        System.out.println(addressbook);
+                        while (menu == 3) {
+                            System.out.println(addressbook);
+                            System.out.println("would you like to do it again? 3: yes, 4: No");
+                            menu = sc.nextInt();
+                        }
                         break;
-
+                    default:
+                        System.out.println("Please Choose an option: ");
+                        System.out.println("(0) Exit.");
+                        System.out.println("(1) Add Contact");
+                        System.out.println("(2) Look up Contact.");
+                        System.out.println("(3) Display all contacts");
+                        menu=sc.nextInt();
                 }
             }
         }
